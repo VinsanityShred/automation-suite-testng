55a453fa1e80e7ca3d5d762328b1bd11bcbb735a
Carlos Ramirez <CarlosRamirez@Gentekss-MacBook-Pro.local>
2021-04-07 12:19:46 -0700

    Added new social media test

---
+++ src/test/java/web/tests/sculptnation/socialMediaTests/SNSocialMediaLinksTest.java
@@ -0,0 +1,47 @@
+package web.tests.sculptnation.socialMediaTests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import web.pages.sculptnation.SNLandingPage;
+import web.tests.BaseTest;
+
+@Feature("Header Tests")
+@Story("Social media links test")
+@Listeners( framework.testng.AllureScreenshots.class )
+public class SNSocialMediaLinksTest extends BaseTest {
+
+    SNLandingPage sNLndPg;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriver driver = getDriver();
+        sNLndPg = new SNLandingPage(driver);
+    }
+
+    @Description("Verify Facebook social media link")
+    @Test()//UUID=EB5572AC-C76A-49C4-BE80-010C2B1632BE
+    public void facebookLinkTest() throws Exception {
+
+        sNLndPg.verifyLandingPgLogoIsDisplayed();
+        sNLndPg.closeDiscountModalDisplayed();
+        sNLndPg.clickFBMediaLink();
+        sNLndPg.verifyFBLogoIsDisplayed();
+        sNLndPg.driverNavigateBack();
+    }
+
+    @Description("Verify Instagram social media link")
+    @Test()//UUID=98971DB7-E3AE-490B-8595-40F3A4C83A77
+    public void instagramLinkTest() throws Exception {
+
+        sNLndPg.verifyLandingPgLogoIsDisplayed();
+        sNLndPg.closeDiscountModalDisplayed();
+        sNLndPg.clickInstagramMediaLink();
+        sNLndPg.verifyInstagramLogoIsDisplayed();
+        sNLndPg.driverNavigateBack();
+    }
+}
