94293341ccaeb3a7b88e8872177516651dfd13f5
Carlos Ramirez <CarlosRamirez@Gentekss-MacBook-Pro.local>
2021-05-12 17:59:45 -0700

    Removed unused import and reformatted xml file

--- src/test/java/web/pages/BasePage.java
+++ src/test/java/web/pages/BasePage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 import java.util.ArrayList;
