180113eb5a6572445f24dfe95c4f7ccbf0372670
Carlos Ramirez <CarlosRamirez@Gentekss-MacBook-Pro.local>
2021-05-27 11:35:59 -0700

    Removed unused imports

---
+++ src/test/java/web/tests/sculptnation/homePageTests/SupplementPurchaseEnzymesTest.java
@@ -0,0 +1,12 @@
+package web.tests.sculptnation.homePageTests;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.testng.annotations.Listeners;
+import web.tests.BaseTest;
+
+@Feature("Home Page Tests")
+@Story("Supplement Purchase Enzymes Test")
+@Listeners( framework.testng.AllureScreenshots.class )
+public class SupplementPurchaseEnzymesTest extends BaseTest {
+}
